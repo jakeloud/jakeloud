7cdfd873285ec39bb25480cb1c1fabb58317a5ae
notTGY <microcraft47@gmail.com>
2023-12-03 17:15:14 +0300

    remove old vcs system for ssh key

--- jakeloud/static/index.js
+++ jakeloud/static/index.js
@@ -10,36 +10,12 @@ const getLoginData = () => {
   return { password, email }
 }
 
-const setVCSData = (vcs) => window.localStorage.setItem('vcs', JSON.stringify(vcs))
-
-const getVCSData = () => {
-  try {
-    return JSON.parse(window.localStorage.getItem('vcs')) || []
-  } catch (e) {
-    return []
-  }
-}
-
-/**
- * Sends a request to the API endpoint.
- *
- * @param {string} op - The operation to perform.
- * @param {Object} body - The request body.
- * @return {Promise<Response>}
- */
 const api = async (op, body = {}) =>
   await fetch('/api', {
     method: 'POST',
     body: JSON.stringify({op, ...getLoginData(), ...body}),
   })
 
-/**
- * Button component
- * 
- * @param {string} text
- * @param {function} onClick
- * @returns {HTMLButtonElement}
- */
 const Button = (text, onClick) => {
   const button = document.createElement('button')
   button.innerText = text
@@ -47,13 +23,6 @@ const Button = (text, onClick) => {
   return button
 }
 
-/**
- * Creates a Field element with a label and an input.
- *
- * @param {string} name
- * @param {string} [type='text']
- * @returns {HTMLDivElement}
- */
 const Field = (name, type='text') => {
   const field = document.createElement('div')
   const input = document.createElement('input')
@@ -67,14 +36,6 @@ const Field = (name, type='text') => {
   return field
 }
 
-/**
- * Creates a form element with the given onSubmit function, submitText, and fields.
- *
- * @param {function} onSubmit
- * @param {string} submitText
- * @param {...HTMLElement} fields
- * @return {HTMLFormElement}
- */
 const Form = (onSubmit, submitText, ...fields) => {
   const form = document.createElement('form')
   form.onsubmit = onSubmit
@@ -119,28 +80,10 @@ const handleCreateApp = async (e) => {
 handleUpdateJakeloud = async () => await api('updateJakeloudOp')
 
 add = (options = {}) => {
-  const vcses = getVCSData()
-  const vcsField = document.createElement('div')
-  const vcsSelect = document.createElement('select')
-  const label = document.createElement('label')
-  vcsSelect.id = 'vcs'
-  vcsSelect.name = 'vcs'
-  vcsSelect.append(
-    ...vcses.map(vcs => {
-      const option = document.createElement('option')
-      option.value = `${vcs.user}:${vcs.token}@${vcs.host}`
-      option.innerText = `${vcs.user}@${vcs.host}`
-      return option
-    })
-  )
-  label.for = 'vcs'
-  label.innerText = 'vcs'
-  vcsField.append(label, vcsSelect)
-
   root.innerHTML = ''
   const p = document.createElement('p')
   p.innerText = `Enter github repo in a format "<user|org>/<repo>". Example docker options: "-v /home/jakeloud:/home/jakeloud -e PASSWORD=jakeloud"`
-  root.append(Form(handleCreateApp, 'create app', Field('name'), Field('domain'), vcsField, Field('repo'), Field('docker options'), p))
+  root.append(Form(handleCreateApp, 'create app', Field('name'), Field('domain'), Field('repo'), Field('docker options'), p))
 }
 
 const handleRegisterAllowed = (registerAllowed) => {
@@ -149,9 +92,6 @@ const handleRegisterAllowed = (registerAllowed) => {
 
 const App = (app) => {
   const additional = app.additional ?? {}
-  const vcses = getVCSData()
-  const vcs = vcses.find(vcs => app.vcs === `${vcs.user}@${vcs.host}` || app.vcs === vcs.host)
-  const displayVcs = vcs ? `${vcs.user}:${vcs.token}@${vcs.host}` : null
 
   const wrapper = document.createElement('div')
   const info = document.createElement('pre')
@@ -169,15 +109,15 @@ owner: ${app.email}
       <label for="a">
       Registration allowed
       </label>`
+    const sshKey = document.createElement('p')
+    sshKey.innerText = `ssh-key: ${additional['ssh-key']}`
 
-    wrapper.append(Button('update jakeloud', handleUpdateJakeloud), registrationCheckbox)
+    wrapper.append(Button('update jakeloud', handleUpdateJakeloud), registrationCheckbox, sshKey)
   } else {
-    if (displayVcs) {
-      wrapper.append(Button('full reboot', () => api('createAppOp', {...app, vcs: displayVcs})))
-      wrapper.append(
-        Button('delete app', () => api('deleteAppOp', {...app, vcs: displayVcs})),
-      )
-    }
+    wrapper.append(Button('full reboot', () => api('createAppOp', app)))
+    wrapper.append(
+      Button('delete app', () => api('deleteAppOp', app)),
+    )
   }
   return wrapper
 }
@@ -186,39 +126,12 @@ let Header
 
 const SettingsTab = () => {
   const jakeloudApp = conf.apps.find(app => app.name === 'jakeloud')
-  const vcses = getVCSData()
   
   root.innerHTML = ''
   root.append(
     Header(),
     Button('logout', setLoginData.bind(null, [null, null])),
-    App(jakeloudApp), 
-    Form(
-      (e) => {
-        const data = new FormData(e.target)
-        e.preventDefault()
-        setVCSData(vcses.concat([formDataToJSON(data)]))
-        VCSTab()
-      },
-      'add vcs',
-      Field('user'),
-      Field('token'),
-      Field('host')
-    ),
-    ...vcses.map((vcs, j) => {
-      const wrapper = document.createElement('div')
-      const info = document.createElement('pre')
-      info.innerHTML = `${vcs.user}@${vcs.host}`
-      wrapper.append(
-        document.createElement('hr'),
-        info,
-        Button('delete vcs', () => {
-          setVCSData(vcses.filter((_, i) => i !== j))
-          VCSTab()
-        }),
-      )
-      return wrapper
-    })
+    App(jakeloudApp),
   )
 }
 
